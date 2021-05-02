f8506523e0af7e4f603563ab8380d05c4dae3ea6
fanghaow <2357322866@qq.com>
2021-05-02 16:29:30 +0800

    make a experience upon 1-D vector that tf between json and py

--- Web_project/web_grep_test.py
+++ Web_project/web_grep_test.py
@@ -1,16 +1,39 @@
 #! usr/bin/env python
 import requests
 import matplotlib.pyplot as plt
+import json
 
-url = 'http://1.15.140.205/' # srtp server created by wxf
-try:
-    print('开始运行')
-    str_html = requests.get(url)
-    str_html.encoding = str_html.apparent_encoding
-    str_data = str_html.text
-    print('数据成功爬取')
+class web_data():
+    def __init__(self, url = 'http://1.15.140.205/'): # srtp server created by wxf
+        self.url = url
 
-except:
-    print(Exception)
+    def web_str(self):
+        try:
+            print('Start greping web ......')
+            str_html = requests.get(self.url)
+            str_html.encoding = str_html.apparent_encoding
+            str_data = str_html.text
+            print('Successfully greped data from url ......')
 
-print(str_data)
+        except:
+            print(Exception)
+
+        print('data greped from url :', str_data)
+        return str_data
+
+    def json_tf(self):
+        pass
+
+    def sim_json(self):
+        # sim_data = 
+        pass
+
+    def caculation(self):
+        pass
+
+def main():
+    wd = web_data()
+    text = wd.web_str()
+
+if __name__ == '__main__':
+    main()
