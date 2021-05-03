f929010bd72b4a344a2193ec56af5897442f7c8d
fanghaow <2357322866@qq.com>
2021-05-03 19:10:24 +0800

    take json data from web_server and makes some comment on web_grep_test.py

--- Web_project/web_grep_test.py
+++ Web_project/web_grep_test.py
@@ -3,11 +3,11 @@ import requests
 import matplotlib.pyplot as plt
 import json
 
-class web_data():
+class web_data(): # Get web data from json file 
     def __init__(self, url = 'http://1.15.140.205/'): # srtp server created by wxf
         self.url = url
 
-    def web_str(self):
+    def web_str(self): # To get web html string
         try:
             print('Start greping web ......')
             str_html = requests.get(self.url)
@@ -21,14 +21,14 @@ class web_data():
         print('data greped from url :', str_data)
         return str_data
 
-    def json_tf(self):
+    def json_tf(self): # Download json file from server and make a transfer
         pass
 
-    def sim_json(self):
+    def sim_json(self): # Proceed local json file 
         # sim_data = 
         pass
 
-    def caculation(self):
+    def caculation(self): # our math model to analyze data
         pass
 
 def main():
