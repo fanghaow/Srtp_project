8199c63914442b231e8b1018f6dd9d5f1c4f0293
fanghaow <2357322866@qq.com>
2021-05-20 01:51:07 +0800

    save modeling process

--- Software_project/junior_gui/My_module/calibration.py
+++ Software_project/junior_gui/My_module/calibration.py
@@ -9,6 +9,7 @@ class CaliBration():
         self.path = 'Software_project/junior_gui/DATA/'
         self.load_label()
         self.load_txt()
+        self.data_tf()
 
     def load_label(self):
         self.grape_num = 20
@@ -20,10 +21,11 @@ class CaliBration():
         for i in range(self.grape_num):
             for j in range(self.pos):
                 label[i, j] = float(table.cell(i+1, j+1).value)
-        self.label = label
+        self.label_mat = label
+        # print('Label shape :', self.label.shape)
 
     def load_txt(self):
-        self.grape_data = np.zeros((self.grape_num, self.pos, 256))
+        self.data_mat = np.zeros((self.grape_num, self.pos, 256))
         for i in range(1, 21):
             for j in range(1, 4):
                 file_name = str(i) + '-' + str(j)
@@ -39,39 +41,46 @@ class CaliBration():
                 # print(file_name)
                 # print(mymatrix)
                 # print('Matrix shape :', mymatrix.shape)
-                self.grape_data[i-1, j-1, :] = np.mean(mymatrix)
+                self.data_mat[i-1, j-1, :] = np.mean(mymatrix)
 
-        print('Grape data shape:', self.grape_data.shape)
+        # print('Grape data shape:', self.data_mat.shape)
 
-    def PLS(self):
-        pass
-
-    def OLS(self):
-        # Load data
+    def data_tf(self): 
+        # Transform data into modeling size
         data = np.zeros((self.grape_num * self.pos, 256))
         label = np.zeros((self.grape_num * self.pos, 1))
         for i in range(self.grape_num):
             for j in range(self.pos):
-                if self.label[i, j] == 0:
+                if self.label_mat[i, j] == 0:
                     wrong_index = i * self.pos + j
-                data[i * self.pos + j] = self.grape_data[i, j, :]
-                label[i * self.pos + j] = self.label[i, j]
-        label = np.delete(label, wrong_index, axis=0)
+                data[i * self.pos + j] = self.data_mat[i, j, :]
+                label[i * self.pos + j] = self.label_mat[i, j]
+        self.label = np.delete(label, wrong_index, axis=0)
         data = np.hstack((np.ones((self.grape_num * self.pos, 1)), data))
-        data = np.delete(data, wrong_index, axis=0)
-        # print('Their shape :', label.shape, data.shape)
+        self.data = np.delete(data, wrong_index, axis=0)
+        # print('Their shape :', self.label.shape, self.data.shape)
+
+    def PLS(self):
+        # Parameters
+        params = np.zeros((256+1, 1))
+
+
+    def OLS(self):
         # Parameters initial setting
         params = np.zeros((256+1, 1))
         # Analytical solution
-        params = np.linalg.pinv(np.dot(data.T, data)) 
-        params = np.dot(params, np.dot(data.T, label))
+        params = np.linalg.pinv(np.dot(self.data.T, self.data)) 
+        params = np.dot(params, np.dot(self.data.T, self.label))
         # Estimate model
-        pre_label = np.dot(data, params)
-        SSR = np.sqrt(np.sum((pre_label - np.mean(label)) ** 2))
-        SST = np.sqrt(np.sum((label - np.mean(label)) ** 2))
+        pre_label = np.dot(self.data, params)
+        print('Error :', np.mean(self.label) - np.mean(pre_label))
+        if np.mean(self.label) == np.mean(pre_label):
+            print('OLS dubuging successfully!!!')
+        SSR = np.sqrt(np.sum((pre_label - np.mean(self.label)) ** 2))
+        SST = np.sqrt(np.sum((self.label - np.mean(self.label)) ** 2))
         R_2 = SSR / SST
         print('R^2 :', R_2)
-        plt.plot(label, 'b', label='Real label')
+        plt.plot(self.label, 'b', label='Real label')
         plt.plot(pre_label, 'r', label='Predict label')
         plt.title('OLS Regression Results')
         plt.xlabel('Sample order')
@@ -85,7 +94,6 @@ class CaliBration():
 
 def main():
     cb = CaliBration()
-    print('Label shape :', cb.label.shape)
     cb.OLS()
 
 if __name__ == '__main__':
