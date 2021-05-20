7a6a209499f873089257493aa966e1e6c3868c35
fanghaow <2357322866@qq.com>
2021-05-20 21:20:13 +0800

    Debug for OLS success and discover OLS is not suitable beceuse data number is not enough!!

--- Software_project/junior_gui/My_module/calibration.py
+++ Software_project/junior_gui/My_module/calibration.py
@@ -23,6 +23,9 @@ class CaliBration():
             for j in range(self.pos_num):
                 try:
                     label[i, j] = float(table.cell(i+1, j+1).value)
+                    if label[i, j] == 0:
+                        label[i, j] = float('inf')
+                        print('There is a zero in (%d, %d)' %(i, j))
                 except:
                     label[i, j] = float('inf')
                     print('There is a bug in (%d, %d)' %(i, j))
@@ -33,7 +36,7 @@ class CaliBration():
     def load_txt(self, grape_num=20, symbol=',', start=0, path='Software_project/junior_gui/DATA/'):
         data_mat = np.zeros((self.grape_num, self.pos_num, 256))
         for i in range(1, grape_num+1):
-            for j in range(1, 4):
+            for j in range(1, self.pos_num+1):
                 file_name = str(i) + '-' + str(j)
                 with open(path + file_name + '.txt', encoding = 'utf-8', mode = 'r') as file:
                     data = file.readlines()
@@ -48,7 +51,6 @@ class CaliBration():
                 # print(mymatrix)
                 # print('Matrix shape :', mymatrix.shape)
                 # print(np.mean(mymatrix, axis=0).shape)
-                print(file_name)
                 data_mat[i-1, j-1, :] = np.mean(mymatrix, axis=0)
 
         # print('Grape data shape:', self.data_mat.shape)
@@ -67,9 +69,9 @@ class CaliBration():
                 label[i * self.pos_num + j, :] = label_mat[i, j]
         if len(wrong_indices) != 0:
             label = np.delete(label, wrong_indices, axis=0)
-            data = np.hstack((np.ones((self.grape_num * self.pos_num, 1)), data))
             data = np.delete(data, wrong_indices, axis=0)
-        print('Their shape :', data.shape, label.shape)
+        data = np.hstack((np.ones((self.grape_num * self.pos_num - len(wrong_indices), 1)), data))
+        print('After transform, their shape trun to :', data.shape, label.shape)
         if needsave:
             # Save data into excel workbook
             style0 = xlwt.easyxf('font: name Times New Roman, color-index black, bold on',\
@@ -94,17 +96,20 @@ class CaliBration():
 
     def OLS(self, data, label):
         # Parameters initial setting
-        params = np.zeros((256+1, 1))
+        vector_num = data.shape[1] + 1
+        params = np.zeros((vector_num, 1))
         # Analytical solution
         params = np.linalg.pinv(np.dot(data.T, data)) 
         params = np.dot(params, np.dot(data.T, label))
-        print('Parameters :', params)
+        # print('Parameters :', params)
         # Estimate model
         pre_label = np.dot(data, params)
-        print('Error between label and predicted label:', np.mean(label) - np.mean(pre_label))
-        print('Mean error between them :', np.mean(np.abs(pre_label - label)))
-        if abs(np.mean(label) - np.mean(pre_label)) <= 1e-6:
-            print('OLS debuging successfully!!!')
+        # print('Label :', label)
+        # print('Predicted label :', pre_label)
+        print('Trainging set error between label and predicted label:', np.mean(label) - np.mean(pre_label))
+        print('Mean abs error between them :', np.mean(np.abs(pre_label - label)))
+        if np.mean(np.abs(pre_label - label)) <= 1e-6:
+            print('OLS performs greatly!!!')
         SSR = np.sqrt(np.sum((pre_label - np.mean(label)) ** 2))
         SST = np.sqrt(np.sum((label - np.mean(label)) ** 2))
         R_2 = SSR / SST
@@ -120,26 +125,54 @@ class CaliBration():
         return params
 
     def predict(self, params):
-        pre_path = 'Software_project/junior_gui/0519DATA/'
-        label_mat = self.load_label(grape_num=36, path=pre_path, filename='realData0519.xlsx')
-        data_mat = self.load_txt(grape_num=36, path=pre_path)
-        data, label = self.data_tf(data_mat, label_mat, needsave=False)
+        print('\nStart predicting!!!\n')
+        # pre_path = 'Software_project/junior_gui/0519DATA/'
+        # label_mat = self.load_label(grape_num=36, path=pre_path, filename='realData0519.xlsx')
+        # data_mat = self.load_txt(grape_num=36, path=pre_path)
+        # data, label = self.data_tf(data_mat, label_mat, needsave=False)
+        # pre_label = np.dot(data, params)
+        label_mat = self.load_label()
+        data_mat = self.load_txt(symbol='\←◆*', start=17)
+        data, label = self.data_tf(data_mat, label_mat)
+        print('Test shape :', data.shape, label.shape)
         pre_label = np.dot(data, params)
-        print('Error between label and predicted label:', np.mean(label) - np.mean(pre_label))
-        print('Mean error between them :', np.mean(np.abs(pre_label - label)))
+
+        print('Test set error between label and predicted label:', np.mean(label) - np.mean(pre_label))
+        print('Mean abs error between them :', np.mean(np.abs(pre_label - label)))
         plt.plot(label, 'b', label='Real label')
         plt.plot(pre_label, 'r', label='Predict label')
         plt.legend()
         plt.show()
 
+def OLS_debug():
+    # Debugging OLS
+    one = np.ones((1, 10))
+    x = np.arange(1, 11)
+    y = x ** 2
+    label = np.reshape((1 + 3 * x + 10 * y + 10 * np.random.rand(1, 10)), (10, 1))
+    data = np.vstack((one, x, y))
+    print(data)
+    print(label.shape)
+    cb = CaliBration()
+    params = cb.OLS(data.T, label)
+    print(params)
+
 def main():
     cb = CaliBration()
-    label_mat = cb.load_label()
-    data_mat = cb.load_txt(symbol='\←◆*', start=17)
-    data, label = cb.data_tf(data_mat, label_mat)
+    # label_mat = cb.load_label()
+    # data_mat = cb.load_txt(symbol='\←◆*', start=17)
+    # data, label = cb.data_tf(data_mat, label_mat)
+    # params = cb.OLS(data, label)
+    # cb.predict(params)
+
+    pre_path = 'Software_project/junior_gui/0519DATA/'
+    label_mat = cb.load_label(grape_num=36, path=pre_path, filename='realData0519.xlsx')
+    data_mat = cb.load_txt(grape_num=36, path=pre_path)
+    data, label = cb.data_tf(data_mat, label_mat, needsave=False)
+    print('Training data shape :', data.shape, label.shape)
     params = cb.OLS(data, label)
-    print(params)
     cb.predict(params)
 
 if __name__ == '__main__':
     main()
+    # OLS_debug()
